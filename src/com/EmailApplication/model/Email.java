7306f411a6d6bf73e72451867c663679fb41c3c5
devjosegomez <devjosegomez@gmail.com>
2019-10-27 11:58:25 -0600

    Added getters and setters of Email's Department

--- src/com/EmailApplication/model/Email.java
+++ src/com/EmailApplication/model/Email.java
@@ -1,5 +1,7 @@
 package com.EmailApplication.model;
 
+import java.util.Scanner;
+
 public class Email {
 	//Encapsulating properties
 	private String firstName;
@@ -17,5 +19,46 @@ public class Email {
 	public Email(String firstName, String lastName){
 		this.firstName = firstName;
 		this.lastName = lastName;
+		
+		//Calling method to set the department
+		this.department = this.setDepartment();
+	}
+	
+	//getters and setters [Department]
+	public String getDepartment() {
+		return this.department;
+	}
+	
+	private String setDepartment() {
+		String department = "";
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter your department:\n1-Sales\n2-Development\n3-Accounting\n4-None");
+		department = input.next();
+		
+		//Sales\n2-Development\n3Accounting\n4-None"
+		switch(department) {
+		case "1":
+			department = "Sales";
+
+			break;
+		case "2":
+			department = "Development";
+
+			break;
+		case "3":
+			department = "Accounting";
+
+			break;
+		case "4":
+			department = "None";
+
+			break;
+		default:
+			System.err.println("Invalid option");
+			department = "";
+			break;
+		}
+		input.close();
+		return department;
 	}
 }
